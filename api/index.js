68d9114d5450267c2ec1e0f1ec5f2734a3e29a14
rayyanworld <businessworld9160@gmail.com>
2024-11-14 03:05:20 -0800

    Add files via upload

---
+++ api/index.js
@@ -0,0 +1,14 @@
+class rayyan{
+    demo(){
+        document.write("rayyan"+"<br>")
+    }
+    test(){
+        document.write("api")
+    }
+    constructor(){
+        document.write("arif"+"<br>")
+    }
+}
+var a= new rayyan()
+a.demo()
+a.test()
